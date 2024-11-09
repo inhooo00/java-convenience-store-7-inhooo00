367c68b01adf2dafc031084de308b0c1eaa9633b
inhooo00 <chldlsgh0987@naver.com>
2024-11-10 00:50:05 +0900

    feat(util): edit Application class
    edit Application class :
    - new ConvenienceStoreController().run();로 시작하는 로직 추가.

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,10 @@
 package store;
 
+import store.controller.ConvenienceStoreController;
+
 public class Application {
     public static void main(String[] args) {
+        new ConvenienceStoreController().run();
         // TODO: 프로그램 구현
     }
 }
