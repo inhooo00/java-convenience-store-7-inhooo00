1ef6dd20981b46f646749f9e241d7a43801fc567
inhooo00 <chldlsgh0987@naver.com>
2024-11-10 20:30:06 +0900

    feat(model): edit InputView class
    edit InputView class :
    - 예외처리 로직 수정.

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
                 System.out.print("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1]): ");
                 String input = Console.readLine();
                 if (!InputValidator.isValidProductSelection(input)) {
-                    throw new IllegalArgumentException("[ERROR] 잘못된 상품명 또는 수량 형식입니다.");
+                    throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
                 }
                 return input;
             } catch (IllegalArgumentException e) {
